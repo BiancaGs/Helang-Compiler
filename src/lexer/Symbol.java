1cdd81d6407057df034798b42c287ec216d1851d
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2021-09-19 17:27:17 -0300

    Setting Symbols

--- src/lexer/Symbol.java
+++ src/lexer/Symbol.java
@@ -1,5 +1,49 @@
 package lexer;
 
-public class Symbol {
-    
+public enum Symbol {
+
+    EOF("eof"), 
+    INT("Int"),
+    IDENT("Ident"), 
+    SEMICOLON(";"), 
+    ASSIGN("="), 
+    IF("if"), 
+    ELSE("else"), 
+    FOR("for"),
+    IN("in"),
+    DOTDOT(".."),
+    PRINT("print"), 
+    PRINTLN("println"),
+    LEFTBRACE("{"),
+    RIGHTBRACE("}"),
+    WHILE("while"),
+    OR("||"), 
+    AND("&&"), 
+    LEFTPAR("("), 
+    RIGHTPAR(")"), 
+    NOT("!"),
+    LT("<"), 
+    LE("<="),
+    GT(">"), 
+    GE(">="), 
+    EQ("=="), 
+    NEQ("!="),
+    PLUS("+"), 
+    MINUS("-"),  
+    MULT("*"), 
+    DIV("/"), 
+    REMAINDER("%"),
+    DIGIT("Digit");
+
+    Symbol(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    private String name;
+
 }
